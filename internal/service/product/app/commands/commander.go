cda533edc26c4314a9bd48569969b6c60ad78abf
daioru <daioru.xyz@gmail.com>
2025-01-28 15:55:06 +0300

    Move HandleUpdate method to default.go

--- internal/service/product/app/commands/commander.go
+++ internal/service/product/app/commands/commander.go
@@ -20,20 +20,3 @@ func NewCommander(bot *tgbotapi.BotAPI,
 		productService: productService,
 	}
 }
-
-func (c *Commander) HandleUpdate(update tgbotapi.Update) {
-	if update.Message == nil { // ignore any non-Message Updates
-		return
-	}
-
-	switch update.Message.Command() {
-	case "help":
-		c.Help(update.Message)
-	case "list":
-		c.List(update.Message)
-	case "get":
-		c.Get(update.Message)
-	default:
-		c.Default(update.Message)
-	}
-}
--- internal/service/product/app/commands/default.go
+++ internal/service/product/app/commands/default.go
@@ -12,4 +12,21 @@ func (c *Commander) Default(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
 
 	c.bot.Send(msg)
-} 
+}
+
+func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	if update.Message == nil { // ignore any non-Message Updates
+		return
+	}
+
+	switch update.Message.Command() {
+	case "help":
+		c.Help(update.Message)
+	case "list":
+		c.List(update.Message)
+	case "get":
+		c.Get(update.Message)
+	default:
+		c.Default(update.Message)
+	}
+}
